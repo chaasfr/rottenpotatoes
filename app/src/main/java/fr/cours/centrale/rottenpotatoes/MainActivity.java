054a2c1bbf4b028739a4ee96f345b90cfafe8753
chaasfr <chaasfr@gmail.com>
2016-02-18 07:06:16 +0100

    Handle internet issues and offline DB.

--- app/src/main/java/fr/cours/centrale/rottenpotatoes/MainActivity.java
+++ app/src/main/java/fr/cours/centrale/rottenpotatoes/MainActivity.java
@@ -1,24 +1,29 @@
 package fr.cours.centrale.rottenpotatoes;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private static String TAG = MainActivity.class.getSimpleName();
     private DBHelper rottenDB;
+    ListView mListView;
+    String[] prenoms = new String[]{
+            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
+            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
+            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
+            "Sophie", "Tristan", "Ulric", "Vincent", "Willy", "Xavier",
+            "Yann", "Zoé"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +33,6 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         rottenDB= new DBHelper(this);
-        Log.d(TAG, rottenDB.getAllseances().get(0) );
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -87,13 +82,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.affiche) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.prochainement) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.evenements) {
+
+        } else if (id == R.id.preferences) {
 
         } else if (id == R.id.nav_share) {
 
